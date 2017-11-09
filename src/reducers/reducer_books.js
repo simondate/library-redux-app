59e23d8262bf9718148a754adb97d9eae084ec95
Simon  Date <simondate@yahoo.co.uk>
2017-11-09 15:13:50 +0000

    Further work

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,6 +1,7 @@
 export default function() {
     return [
-      {title: 'book 1'},
-      {title: 'book 2'}
+      {title: 'book 1', pages: 50},
+      {title: 'book 2', pages: 22},
+      {title: 'book 3', pages: 222},
     ]
 }
